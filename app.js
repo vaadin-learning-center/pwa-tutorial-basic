dba4f06d2aa1fe6cadddf097fc4f58a04976f234
Marcus Hellberg <marcus@vaadin.com>
2018-04-04 15:39:49 -0700

    Base application without any PWA functionality

---
+++ app.js
@@ -0,0 +1,71 @@
+class PWAConfApp {
+  constructor() {
+    this.speakersDiv = document.querySelector('#speakers');
+    this.scheduleDiv = document.querySelector('#schedule');
+    this.init();
+  }
+
+  async init() {
+    await this.loadSpeakers();
+    await this.loadSchedule();
+  }
+  async loadSpeakers() {
+    this.speakers = await this.fetchJSON('/speakers.json');
+
+    this.speakersDiv.innerHTML = this.speakers
+      .map(this.toSpeakerBlock)
+      .join('\n');
+  }
+
+  async loadSchedule() {
+    const rawSchedule = await this.fetchJSON('/schedule.json');
+
+    // Add speaker details to array
+    this.schedule = rawSchedule.map(this.addSpeakerDetails, this);
+    this.scheduleDiv.innerHTML = this.schedule
+      .map(this.toScheduleBlock)
+      .join('\n');
+  }
+
+  toSpeakerBlock(speaker) {
+    return `
+        <div class="speaker">
+          <img src="${speaker.picture}" alt="${speaker.name}">
+          <div>${speaker.name}</div>
+        </div>`;
+  }
+
+  toScheduleBlock(scheduleItem) {
+    return `
+      <div class="schedule-item ${scheduleItem.category}">
+        <div class="title-and-time">
+          <div class="time">${scheduleItem.startTime}</div>
+          <div class="title-and-speaker">
+            <div class="title">${scheduleItem.title}</div>
+            <div class="speaker">${
+              scheduleItem.speaker ? scheduleItem.speaker.name : '&nbsp;'
+            }</div>
+          </div>
+        </div>
+        <p class="description">${scheduleItem.description}</p>
+      </div>
+    `;
+  }
+
+  addSpeakerDetails(item) {
+    if (item.speakerId) {
+      return Object.assign({}, item, {
+        speaker: this.speakers.find(s => s.id === item.speakerId)
+      });
+    }
+    return Object.assign({}, item);
+  }
+
+  async fetchJSON(url) {
+    const res = await fetch(url);
+    return res.json();
+  }
+}
+window.addEventListener('load', e => {
+  new PWAConfApp();
+});
