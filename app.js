92b980335fce7c47faf7606073ba36fa1978dff9
Marcus Hellberg <marcus@vaadin.com>
2018-04-08 17:05:51 +0300

    starting point for tutorial

--- app.js
+++ app.js
@@ -100,17 +100,4 @@ class PWAConfApp {
 }
 window.addEventListener('load', e => {
   new PWAConfApp();
-  registerSW();
 });
-
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./sw.js');
-    } catch (e) {
-      alert('ServiceWorker registration failed. Sorry about that.');
-    }
-  } else {
-    document.querySelector('.alert').removeAttribute('hidden');
-  }
-}
